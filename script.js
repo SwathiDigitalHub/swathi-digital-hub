458a63c52266a44746db02526972a4ba1d977528
SwathiDigitalHub <vankayalaswathi9@gmail.com>
2026-06-26 18:44:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to Swathi Digital Hub!");
+
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", function () {
+        alert("Welcome to Swathi Digital Hub! Redirecting to WhatsApp...");
+    });
+});
