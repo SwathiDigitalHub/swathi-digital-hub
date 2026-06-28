73e19cf5870f5d502cffd3c492c4cb9450e7f4a3
SwathiDigitalHub <vankayalaswathi9@gmail.com>
2026-06-28 18:16:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Welcome to Swathi Digital Hub!");
-
-    const button = document.querySelector("button");
-
-    button.addEventListener("click", function () {
-        alert("Welcome to Swathi Digital Hub! Redirecting to WhatsApp...");
-    });
-});
